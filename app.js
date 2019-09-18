2e593db3bbe151eb192995784cd6f9cd2d61705f
Jiang Ng <jiang.ng7@gmail.com>
2019-09-18 13:38:45 +0000

    change port number

--- app.js
+++ app.js
@@ -61,7 +61,8 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function() {
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
 	console.log("Yelp Camp Server has started.");
 });
 
